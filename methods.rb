ed735b037a8bfa559d3b2ffdd23ba0c0441fcf36
Tsuyoshi-Toughy <ogutsuyoshi@gmail.com>
2021-01-22 10:13:58 +0900

    each.with_index(1)~end,while~end,if~endそれぞれのインデントを半角スペース2つ分に訂正

--- methods.rb
+++ methods.rb
@@ -21,10 +21,10 @@ def decide_number_of_people(chosen_plan)
   puts "#{chosen_plan[:place]}旅行ですね"
   puts "何名で予約されますか?"
   while true
-  print "人数を入力 >"
-  number_of_people = gets.to_i
-  break if number_of_people>= 1
-  puts "1名以上選択して下さい。"
+    print "人数を入力 >"
+    number_of_people = gets.to_i
+    break if number_of_people>= 1
+    puts "1名以上選択して下さい。"
 end
 number_of_people
 end
@@ -33,8 +33,8 @@ def calculate_charges(chosen_plan,number_of_people)
   puts "#{number_of_people}名ですね"
   total_price = chosen_plan[:price] * number_of_people
   if number_of_people >= 5
-  puts "5名以上ですので10%割引となります。"
-  total_price*= 0.9
+    puts "5名以上ですので10%割引となります。"
+    total_price*= 0.9
   end
   puts "合計料金は#{total_price.floor}円になります。"
 end
